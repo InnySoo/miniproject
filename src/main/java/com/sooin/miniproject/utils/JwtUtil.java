75b0a1523530e18589cf30d5c1c02598b2a001ef
sooin <sooin0458@gmail.com>
2024-12-05 23:18:01 +0900

    first commit

---
+++ src/main/java/com/sooin/miniproject/utils/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.sooin.miniproject.utils;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION_TIME = 1000 * 60 * 60;
+    
+    public String generateToken(String userName) {
+         
+        String jwt = Jwts.builder()
+            .signWith(SECRET_KEY)
+            .setSubject(userName)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .compact();
+
+        return jwt;
+    }
+
+    public String validate(String jwt) {
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return claims.getSubject();
+
+    }
+}
